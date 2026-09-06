fdc7cb4907eeeae70ca2f6bb60fc41b88fe22f50
B. van Berkum <dev@dotmpe.com>
2026-09-06 03:20:51 +0200

    Keeping GH workflow copy on default branch

---
+++ tool/local/ubuntu_ci_install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-get install -y shellcheck
+
+# Install bashunit
+curl -s https://bashunit.com/install.sh | bash
+
+# Install redo from dotmpe/redo
+REDO_TMP="$(mktemp -d)"
+git clone --depth 1 https://github.com/dotmpe/redo.git "$REDO_TMP"
+(
+  cd "$REDO_TMP"
+  ./do -j10 build
+  sudo DESTDIR= PREFIX=/usr/local ./do -j10 install
+)
+sudo rm -rf "$REDO_TMP"
