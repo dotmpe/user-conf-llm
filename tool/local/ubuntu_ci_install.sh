124c894f020e2a8de2bd3bc123e951c29a3654ef
B. van Berkum <dev@dotmpe.com>
2026-09-06 04:02:33 +0200

    Changed redo version

--- tool/local/ubuntu_ci_install.sh
+++ tool/local/ubuntu_ci_install.sh
@@ -8,7 +8,7 @@ curl -s https://bashunit.com/install.sh | bash
 
 # Install redo from dotmpe/redo
 REDO_TMP="$(mktemp -d)"
-git clone --depth 1 https://github.com/dotmpe/redo.git "$REDO_TMP"
+git clone --branch ifdone --depth 1 https://github.com/dotmpe/redo.git "$REDO_TMP"
 (
   cd "$REDO_TMP"
   ./do -j10 build
